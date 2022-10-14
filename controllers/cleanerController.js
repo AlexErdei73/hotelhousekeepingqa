8d1b14efdb61ac81046a602dc440a6e1d3214209
Alex Erdei <mralexerdei@yahoo.co.uk>
2022-10-14 09:43:19 +0100

    Make cleaner updates work

--- controllers/cleanerController.js
+++ controllers/cleanerController.js
@@ -98,3 +98,81 @@ exports.cleaner_get = function(req, res, next) {
         });
     })
 }
+
+exports.cleaner_update_get = function(req, res, next) {
+    Cleaner.findById(req.params.id).exec(function(err, cleaner) {
+        if (err) {
+            return next(err);
+        }
+        if (cleaner===null) {
+            const error = new Error("Cleaner not found");
+            error.status = 404;
+            return next(error);
+        }
+        res.render("cleaner_form", {
+            title: "Update Cleaner",
+            cleaner,
+            errors: null
+        })
+    })
+}
+
+exports.cleaner_update_post = [
+
+    //Form validation and sanitization
+    body("first_name")
+    .trim()
+    .isLength({min: 1})
+    .escape()
+    .withMessage("First Name must be specified")
+    .isAlphanumeric()
+    .withMessage("First Name can only contain alphanumeric characters"),
+    body("last_name")
+    .optional({ checkFalsy: true })
+    .trim()
+    .escape()
+    .isAlphanumeric()
+    .withMessage("Last Name can only contain alphanumeric characters"),
+    body("active")
+    .isIn(['true','false'])
+    .withMessage("Cleaner Active must be either true or false"),
+    body("start_date")
+    .optional({ checkFalsy:true })
+    .isISO8601()
+    .toDate(),
+    body("end_date")
+    .optional({ checkFalsy:true })
+    .isISO8601()
+    .toDate(),
+
+    //this is our middlewear 
+    (req, res, next) => {
+        //extract the validation errors
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            //There are errors, rerender the form
+            res.render("cleaner_form", {
+                title: "Update Cleaner",
+                cleaner: req.body,
+                errors: errors.array(),
+            });
+            return;
+        } 
+        //No errors, so we can save the cleaner in the database
+        const cleaner = new Cleaner({
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            active: req.body.active==="true" ? true : false,
+            start_date: req.body.start_date,
+            end_date: req.body.end_date,
+            _id: req.params.id
+        });
+        Cleaner.findByIdAndUpdate(req.params.id, cleaner, {}, function(err) {
+            if (err) {
+                return next(err);
+            }
+            res.redirect('/hotel/cleaners');
+        })
+    }
+]
--- routes/hotel.js
+++ routes/hotel.js
@@ -11,5 +11,7 @@ router.get('/cleaners', cleaner_controller.cleaner_list);
 router.get('/cleaner/create', cleaner_controller.cleaner_create_get);
 router.post('/cleaner/create', cleaner_controller.cleaner_create_post);
 router.get('/cleaner/:id', cleaner_controller.cleaner_get);
+router.get('/cleaner/update/:id', cleaner_controller.cleaner_update_get);
+router.post('/cleaner/update/:id', cleaner_controller.cleaner_update_post);
 
 module.exports = router;
